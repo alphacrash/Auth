d5e867aa8ab5ef09fd17d178782b21d7796fe368
alphacrash <sudhansu.ace@gmail.com>
2021-12-16 13:39:39 +0530

    Refactored

--- server/services/passport.js
+++ server/services/passport.js
@@ -17,7 +17,6 @@ const localLogin = new LocalStrategy(localOptions, function (
 ) {
   User.findOne({ email: email }, function (err, user) {
     if (err) {
-      console.log("Error: User.findOne: ", err);
       return done(err);
     }
 
@@ -27,11 +26,9 @@ const localLogin = new LocalStrategy(localOptions, function (
 
     user.comparePassword(password, function (err, isMatch) {
       if (err) {
-        console.log("Error: comparePassword: ", err);
         return done(err);
       }
       if (!isMatch) {
-        console.log("Not a Match: ", err);
         return done(null, false);
       }
 
